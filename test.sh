eba4f9e2e937bc0a05a13a0d764fa43f03ca0c6a
Jaewook Ahn <ajw4586@gmail.com>
2021-12-13 04:03:05 +0900

    Improve test script to test all js files in sample directory

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "mini-javascript test script"
-echo
 
 BINARY=./javascript
 if [ ! -f $BINARY ]; then
@@ -9,29 +8,11 @@ if [ ! -f $BINARY ]; then
     exit
 fi
 
-echo "Test variable declaration"
-$BINARY samples/variable.js
-
-echo
-echo "Test condition statements"
-$BINARY samples/condition.js
-
-echo
-echo "Test iteration stastements"
-$BINARY samples/iteration.js
-
-echo
-echo "Test unary expressions"
-$BINARY samples/unary.js
-
-echo
-echo "Test strict mode"
-$BINARY samples/use_strict.js
-
-echo
-echo "Test error file"
-$BINARY samples/err.js
-
-echo
-echo "Test simple javascript source"
-$BINARY samples/simple.js
+SAMPLE_DIR=./samples
+for SRC in "$SAMPLE_DIR"/*.js
+do
+    if [ -f $SRC ]; then
+        echo -e "\nTest $SRC"
+        $BINARY $SRC
+    fi
+done
